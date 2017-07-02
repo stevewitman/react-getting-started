767a50f43c60f9875b70dc149209b2aae87a2216
Steve Witman <stevewitman@gmail.com>
2017-07-01 23:00:09 -0600

    First component

---
+++ README.md
@@ -0,0 +1,4 @@
+https://app.pluralsight.com/player?course=react-js-getting-started&author=samer-buna&name=react-js-getting-started-m1&clip=1&mode=live
+
+https://jscomplete.com/repl
+
---
+++ first.js
@@ -0,0 +1,15 @@
+// const Button = function(props) {
+// 	return (
+//   <button>{props.label}</button>
+//   );
+// };
+
+// ReactDOM.render(<Button label="Do" />, mountNode);
+
+const Button = (props) => {
+	return (
+  <button>Go</button>
+  );
+};
+
+ReactDOM.render(<Button />, mountNode);
